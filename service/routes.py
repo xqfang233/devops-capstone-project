f02b60f9e450e35d2e928fe9c30c37de662c452b
xqfang233 <81652429+xqfang233@users.noreply.github.com>
2024-02-28 23:45:50 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -70,7 +70,7 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
-    
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
